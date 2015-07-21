a67c3fba292fd9dc1fa2afe8719c83ad81e7c362
Olivier Fayau <olivier.fayau@gmail.com>
2015-07-21 22:28:11 +0200

    Ajout de lien vers README

--- genDockerfile.sh
+++ genDockerfile.sh
@@ -9,7 +9,14 @@ for profile in compact1 compact2 compact3 jre; do
 	echo "Generate Dockerfile for $ejdk $profile"
 	# Create Dockerfile
 	sed -e "s/%JRE%/$ejdk/g" -e "s/%PROFILE%/$profile/g" Dockerfile.tpl > ejdk/linux_i586/$profile/Dockerfile
-	# Ignore Dockerfile from copying
-	echo "Dockerfile" > ejdk/linux_i586/$profile/.dockerignore
+	# Link current README.md	
+	ln -sf ../../../README.md ejdk/linux_i586/$profile/
+	# Create .dockerignore to ignore from copying
+	#echo "Dockerfile" > ejdk/linux_i586/$profile/.dockerignore
+	#echo "README.md" >> ejdk/linux_i586/$profile/.dockerignore
+	cat > ejdk/linux_i586/$profile/.dockerignore <<- EOF
+		Dockerfile
+		README.md
+	EOF
 done
 
